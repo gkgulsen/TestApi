769405d47d7f45f2aaded88a870f04edd9e52e9b
GGULSEN\ggulsen <gokhangulsen@ogr.erbakan.edu.tr>
2024-02-08 01:41:19 +0300

    weather cont

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "AFreezing", "BBracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "XFreezing", "XBracing", "XChilly", "XCool", "XMild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
